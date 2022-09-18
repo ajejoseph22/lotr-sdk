f359d985f1a3a9d7c956ea9dbac496fbd3be35cf
Joseph Aje <ajejoseph22@gmail.com>
2022-09-18 13:30:04 +0100

    :construction: extracted base paths into static variable

--- src/services/book.ts
+++ src/services/book.ts
@@ -4,6 +4,7 @@ import {Book as BookType, ListBooksResponse} from "../types/book";
 import {Chapter, ListChaptersResponse} from "../types/chapter";
 
 export default class Book {
+  private static readonly BASE_PATH = "/book"
   private client: AxiosInstance;
 
   constructor(client: AxiosInstance) {
@@ -11,14 +12,14 @@ export default class Book {
   }
 
   list(): Promise<BookType[]> {
-    return this.client.get("/book").then((response: AxiosResponse<ListBooksResponse>) => response.data.docs);
+    return this.client.get(Book.BASE_PATH).then((response: AxiosResponse<ListBooksResponse>) => response.data.docs);
   }
 
   get(id: string): Promise<BookType> {
-    return this.client.get(`/book/${id}`).then((response: AxiosResponse<ListBooksResponse>) => response.data.docs[0]);
+    return this.client.get(`${Book.BASE_PATH}/${id}`).then((response: AxiosResponse<ListBooksResponse>) => response.data.docs[0]);
   }
 
   getChapters(id: string): Promise<Chapter[]> {
-    return this.client.get(`/book/${id}/chapter`).then((response: AxiosResponse<ListChaptersResponse>) => response.data.docs);
+    return this.client.get(`${Book.BASE_PATH}/${id}/chapter`).then((response: AxiosResponse<ListChaptersResponse>) => response.data.docs);
   }
 }
--- src/services/movie.ts
+++ src/services/movie.ts
@@ -4,6 +4,7 @@ import {ListMoviesResponse, Movie as MovieType} from "../types/movie";
 import {ListQuotesResponse, Quote} from "../types/quote";
 
 export default class Movie {
+  private static readonly BASE_PATH = "/movie"
   private client: AxiosInstance;
 
   constructor(client: AxiosInstance) {
@@ -11,14 +12,14 @@ export default class Movie {
   }
 
   list(): Promise<MovieType[]> {
-    return this.client.get("/movie").then((response: AxiosResponse<ListMoviesResponse>) => response.data.docs);
+    return this.client.get(Movie.BASE_PATH).then((response: AxiosResponse<ListMoviesResponse>) => response.data.docs);
   }
 
   get(id: string): Promise<MovieType> {
-    return this.client.get(`/movie/${id}`).then((response: AxiosResponse<ListMoviesResponse>) => response.data.docs[0]);
+    return this.client.get(`${Movie.BASE_PATH}/${id}`).then((response: AxiosResponse<ListMoviesResponse>) => response.data.docs[0]);
   }
 
   getQuotes(id: string): Promise<Quote[]> {
-    return this.client.get(`/movie/${id}/quote`).then((response: AxiosResponse<ListQuotesResponse>) => response.data.docs);
+    return this.client.get(`${Movie.BASE_PATH}/${id}/quote`).then((response: AxiosResponse<ListQuotesResponse>) => response.data.docs);
   }
 }
